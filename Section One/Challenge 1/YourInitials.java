39ae789612149d595b1ff199908c8cae452b1de1
Koltus96 <116841250+Koltus96@users.noreply.github.com>
2022-11-02 14:56:32 -0700

    Add files via upload

---
+++ Section One/Challenge 1/YourInitials.java
@@ -0,0 +1,13 @@
+public class YourInitials {
+    public static void main(String[] args) {
+         // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("JJJJJ  EEEEE");
+         System.out.println("  J    E    ");
+         System.out.println("  J    E    ");
+         System.out.println("  J    EEE  ");
+         System.out.println("  J    E    ");
+         System.out.println("J J    E    ");
+         System.out.println(" JJ    EEEEE");
+        
+    }
+}

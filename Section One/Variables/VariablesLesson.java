45083d0c3c5e70b6bb0dd11561d2b309db89a7c5
Koltus96 <116841250+Koltus96@users.noreply.github.com>
2022-11-02 16:13:39 -0700

    Add files via upload

---
+++ Section One/Variables/VariablesLesson.java
@@ -0,0 +1,12 @@
+package Variables;
+
+public class VariablesLesson {
+    public static void main(String[] args) {
+        int passengers = 20;
+        System.out.println(passengers);
+
+        int busTickets = passengers;
+        System.out.println(busTickets);
+    }
+    
+}

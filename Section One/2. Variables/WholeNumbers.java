cab50cb6ad0dc30944b89e1534a74c66e6abba82
Jake Elston <116841250+Koltus96@users.noreply.github.com>
2022-11-06 16:38:02 -0800

    Add files via upload

---
+++ Section One/2. Variables/WholeNumbers.java
@@ -0,0 +1,13 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        int numberOfFans = 5000;
+        System.out.println("There are " + numberOfFans + " number of fans at the game");
+
+        long globalPopulation = 7000000000L;
+        System.out.println("There are " + globalPopulation + " people on earth");
+
+        long dailyGoogleSearches = 5000000000L;
+        System.out.println("There are " + dailyGoogleSearches + " Google searches per day");
+    }
+    
+}

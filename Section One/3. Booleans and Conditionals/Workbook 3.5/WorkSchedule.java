2fd6037a2a2efd6d3c4bb5a03ebb3acf5d9a6768
Jake Elston <116841250+Koltus96@users.noreply.github.com>
2022-11-15 15:52:20 -0800

    Add files via upload

--- Section One/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
+++ Section One/3. Booleans and Conditionals/Workbook 3.5/WorkSchedule.java
@@ -3,11 +3,18 @@ public class WorkSchedule {
         
         //See Learn the Part for the complete instructions (link in resources folder of Udemy video).  
 
-        int day = 3;   //3rd day of the week...
-        boolean holiday = true;
-         
+        int day = 6;   //3rd day of the week...
+        boolean holiday = false;
+         // Monday = 1, Sunday =7
         // IF - ELSE IF - ELSE HERE!
         
+        if (holiday){
+            System.out.println("It's a holiday");
+        } else if (day == 6 || day == 7) {
+            System.out.println("It's the weekend");
+        } else {
+            System.out.println("Wake up at 7am");
+        }
         
         }
 
